a7ed95925231f81be55a64bce0fd59e1b0438072
Caitlin Grasso <caitlingrasso@caitlins-mbp-2.lan>
2023-10-27 16:46:34 -0400

    wip

---
+++ network_analysis/degree_distributions.m
@@ -0,0 +1,27 @@
+clearvars
+close all
+clc
+
+addpath('./BCT/2019_03_03_BCT/');
+
+
+bots = {'bot_01', 'bot_02', 'bot_03', 'bot_04', 'bot_05', 'bot_06'};
+
+
+for i = 1:length(bots)
+
+    bot = string(bots(i));
+    
+    % read in functional connectivity matrix
+    FC1 = readmatrix(strcat('../network_inference_data/fc_matrices/', bot, '_before_mimat_w.csv'));
+    FC2 = readmatrix(strcat('../network_inference_data/fc_matrices/', bot, '_after_mimat_w.csv'));
+
+    % before
+    deg1 = sort(degrees_und(FC1), 'descend');
+    writematrix(deg1, strcat('../network_analysis_data/degree_distributions/',bot,'_before_degree_distribution.csv'))
+    
+    % after
+    deg2 = sort(degrees_und(FC2), 'descend');
+    writematrix(deg2, strcat('../network_analysis_data/degree_distributions/',bot,'_after_degree_distribution.csv'))
+    
+end
